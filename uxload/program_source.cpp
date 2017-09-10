ff3b0090409fafa003aee95f619c722972acafed
lostjared <jaredbruni@gmail.com>
2017-09-09 19:40:15 -0700

    removed comment

--- uxload/program_source.cpp
+++ uxload/program_source.cpp
@@ -30,7 +30,7 @@ namespace loader {
                 struct sockaddr_in *s = (struct sockaddr_in *)&addr;
                 port = ntohs(s->sin_port);
                 inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof(ipstr));
-            } else { // AF_INET6
+            } else {
                 struct sockaddr_in6 *s = (struct sockaddr_in6 *)&addr;
                 port = ntohs(s->sin6_port);
                 inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof(ipstr));
